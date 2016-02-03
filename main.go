2406c85b683d4fab2f1dcb23d37c1e6529422f55
Travis Scholl <tscholl2@uw.edu>
2016-02-02 22:48:19 -0800

    removed dependency, added base 2 support

--- main.go
+++ main.go
@@ -2,35 +2,47 @@ package main
 
 import (
 	"encoding/base64"
+	"flag"
 	"fmt"
+	"log"
 
-	"github.com/alexflint/go-arg"
 	"github.com/tscholl2/goprime"
 )
 
 func main() {
+	flag.CommandLine.Usage = func() {
+		fmt.Println(`generate a prime and print to stdout
+Example: 'prime -b 128 -f 16' ---> f72f3e72feb9cc3e030a6a5c8873dd49
+Example: 'prime -b 256 -f 64' ---> /ICHR9xHQ843HisAED723qiesV1LtzxHEzSFPOPL3bc=
+Options:`)
+		flag.CommandLine.PrintDefaults()
+	}
 	args := struct {
-		Bits    uint `arg:"-b,help:number of bits"`
-		Format  uint `arg:"-f,help:give output in base f"`
-		Newline bool `arg:"-n,help:set to remove newline"`
-	}{256, 10, false}
-	arg.MustParse(&args)
-	if args.Bits == 0 {
-		fmt.Println("error: bits must be positive integer")
-		return
+		Bits   uint
+		Format uint
+	}{}
+	flag.UintVar(&args.Bits, "b", 256, "number of bits [supports: 2,...,255,...]")
+	flag.UintVar(&args.Format, "f", 10, "format of output [supports: 2,10,16,64]")
+	flag.Parse()
+	if args.Bits <= 1 {
+		log.Fatalf("error: bits must be positive integer > 1, not %d\n", args.Bits)
 	}
 	p := goprime.RandPrime(int(args.Bits))
 	switch args.Format {
+	case 2:
+		s := ""
+		for p.Sign() != 0 {
+			s = fmt.Sprintf("%d", p.Bits()[0]&1) + s
+			p.Rsh(p, 1)
+		}
+		fmt.Println(s)
 	case 10:
-		fmt.Printf("%d", p)
+		fmt.Printf("%d\n", p)
 	case 16:
-		fmt.Printf("%x", p)
+		fmt.Printf("%x\n", p)
 	case 64:
-		fmt.Printf("%s", base64.StdEncoding.EncodeToString(p.Bytes()))
+		fmt.Printf("%s\n", base64.StdEncoding.EncodeToString(p.Bytes()))
 	default:
 		fmt.Printf("error: unknown base")
-	}
-	if !args.Newline {
-		fmt.Printf("\n")
 	}
 }
