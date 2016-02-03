dffcf60175b99e310532466b559915a19549daa7
Travis <tscholl2@uw.edu>
2016-02-03 08:04:13 -0800

    use println instead of manual line, add raw bytes option

--- main.go
+++ main.go
@@ -5,15 +5,17 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/tscholl2/goprime"
 )
 
 func main() {
 	flag.CommandLine.Usage = func() {
-		fmt.Println(`generate a prime and print to stdout
-Example: 'prime -b 128 -f 16' ---> f72f3e72feb9cc3e030a6a5c8873dd49
-Example: 'prime -b 256 -f 64' ---> /ICHR9xHQ843HisAED723qiesV1LtzxHEzSFPOPL3bc=
+		fmt.Println(`prime: generate a prime number and print to stdout
+Example: 'prime -b 128 -f 16' prints: f72f3e72feb9cc3e030a6a5c8873dd49
+Example: 'prime -b 256 -f 64' prints: /ICHR9xHQ843HisAED723qiesV1LtzxHEzSFPOPL3bc=
+Example: 'prime -b 1024 -f 0 > p.bytes' saves the raw bytes to the file 'p.bytes'
 Options:`)
 		flag.CommandLine.PrintDefaults()
 	}
@@ -25,24 +27,27 @@ Options:`)
 	flag.UintVar(&args.Format, "f", 10, "format of output [supports: 2,10,16,64]")
 	flag.Parse()
 	if args.Bits <= 1 {
-		log.Fatalf("error: bits must be positive integer > 1, not %d\n", args.Bits)
+		log.Fatalf("error: bits must be positive integer > 1, not %d", args.Bits)
 	}
 	p := goprime.RandPrime(int(args.Bits))
+	var s string
 	switch args.Format {
+	case 0:
+		os.Stdout.Write(p.Bytes())
+		return
 	case 2:
-		s := ""
 		for p.Sign() != 0 {
 			s = fmt.Sprintf("%d", p.Bits()[0]&1) + s
 			p.Rsh(p, 1)
 		}
-		fmt.Println(s)
 	case 10:
-		fmt.Printf("%d\n", p)
+		s = fmt.Sprintf("%d", p)
 	case 16:
-		fmt.Printf("%x\n", p)
+		s = fmt.Sprintf("%x", p)
 	case 64:
-		fmt.Printf("%s\n", base64.StdEncoding.EncodeToString(p.Bytes()))
+		s = fmt.Sprintf("%s", base64.StdEncoding.EncodeToString(p.Bytes()))
 	default:
-		fmt.Printf("error: unknown base")
+		s = "error: unknown base"
 	}
+	fmt.Println(s)
 }
