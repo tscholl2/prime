4ebbeb0d52e51633079ac05db1a338639d974c54
Travis <tscholl2@uw.edu>
2016-04-24 09:34:49 -0700

    add leq, addition, utils, need 993/1024

--- float/powers.go
+++ float/powers.go
@@ -20,10 +20,17 @@ type fpn struct {
 }
 
 func (r *fpn) normalize() *fpn {
+	if r == nil || r.n == nil || r.n.Bit(0) == 1 {
+		return r
+	}
 	i := trailingZeroBits(r.n)
 	r.a = r.a + int(i)
 	r.n.Rsh(r.n, i)
 	return r
+}
+
+func (r *fpn) isZero() bool {
+	return r == nil || r.n == nil || r.n.BitLen() == 0
 }
 
 func (r *fpn) mul(s, t *fpn) *fpn {
@@ -33,6 +40,43 @@ func (r *fpn) mul(s, t *fpn) *fpn {
 	r.n.Mul(s.n, t.n)
 	r.a = s.a + t.a
 	return r
+}
+
+func (r *fpn) add(s, t *fpn) *fpn {
+	if r.n == nil {
+		r.n = new(big.Int)
+	}
+	s.normalize()
+	t.normalize()
+	m := min(s.a, t.a)
+	s.n.Lsh(s.n, uint(s.a-m))
+	s.a = m
+	t.n.Lsh(t.n, uint(t.a-m))
+	t.a = m
+	r.n.Add(s.n, t.n)
+	r.a = m
+	return r.normalize()
+}
+
+// returns true if r <= 1
+func (r *fpn) leq1() bool {
+	if r == nil || r.n == nil || r.n.Sign() <= 0 {
+		return true
+	}
+	r.normalize()
+	if r.a > 0 {
+		return false
+	}
+	if r.a == 0 {
+		return r.n.BitLen() <= 1
+	}
+	return r.n.BitLen()-1 < -r.a
+}
+
+// returns true if r <= 993/1024
+func (r *fpn) leq993over1024() bool {
+	// TODO
+	panic("unimplemented")
 }
 
 // returns ceil(log_2(k))
@@ -97,36 +141,36 @@ func powb(r *fpn, k uint, b uint) *fpn {
 	return truncb(s3.n, s3.a, b)
 }
 
-// attempt to find a number within b bits of
-// y^(-1/k)
-func nrootbyk(y *big.Float, k, b int64) *big.Float {
-	// TODO
-	return nil
-}
-
-func algB(y *big.Int, k, b int) *big.Float {
-	if b < 1 || b > int(math.Log(8*float64(k))) {
+func algB(y *fpn, k uint, b uint) *fpn {
+	if b == 0 || int(b) > int(math.Log(8*float64(k))) || y.isZero() {
 		panic("no")
 	}
 	// initialization stuff
-	g := y.BitLen()
-	a := -g / k
-	//B := int64(math.Ceil(math.Log(66 * float64(2*k+1))))
-	// 1. set z <- 2^a + 2^(a-1), j <- 1
-	z := new(big.Int).Add(new(big.Int).Lsh(one, uint(a)), new(big.Int).Lsh(one, uint(a-1)))
-	j := 1
-	// 2. if j = b stop
-	if j == b {
-		return new(big.Float).SetInt(z)
+	g := y.n.BitLen() + y.a                        // 2^(g-1) <= y < 2^g, works if y > 0
+	a := int(math.Floor(float64(-g) / float64(k))) // floor(-g/k)
+	B := uint(logCeil(66 * (2*k + 1)))             // ceil(log(66(2k+1)))
+	// 1. set z = 2^a + 2^(a-1) and j = 1
+	z := &fpn{big.NewInt(3), a - 1}
+	for j := uint(1); j < b; j++ {
+		// 2. if j = b stop
+		if j == b {
+			return z
+		}
+		// 3. compute r <- truncB(powB(z,k),truncB(y))
+		pz := new(fpn).mul(powb(z, k, B), truncb(y.n, y.a, B))
+		r := truncb(pz.n, pz.a, B)
+		// 4. if r <= 993/1024 then set z = z + 2^{a - j - 1}
+		if r.leq993over1024() {
+			// TODO
+		}
+		// 5. if r > 1 then set z = z - 2^{a - j - 1}
+		if !r.leq1() {
+			// TODO
+		}
+		// 6. set j = j + 1 and go to step 2
+		// done in the for loop
 	}
-	// 3. compute r <- truncB(powB(z,k),truncB(y))
-	r := 0.5
-	// 4.
-	if r < 993.0/1024.0 {
-		z = z.Add(z, new(big.Int).Lsh(one, uint(a-j-1)))
-	}
-
-	//TODO
+	panic("hmmmm")
 	return nil
 }
 
@@ -168,4 +212,11 @@ func trailingZeroBits(x *big.Int) (i uint) {
 	for i = 1; i < uint(x.BitLen()) && x.Bit(int(i)) != 1; i++ {
 	}
 	return
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
