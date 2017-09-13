89226afd26ab9a85a893ea5316b00a18b862c209
Travis Scholl <tscholl2@uw.edu>
2017-09-13 08:40:41 -0700

    working lsh and initial set

--- number/natural.go
+++ number/natural.go
@@ -2,54 +2,63 @@ package number
 
 import (
 	"encoding/hex"
-	"fmt"
+	"math"
 )
 
 type number []uint64
 
-func (x *number) setHex(s string) *number {
+func (z number) setHex(s string) number {
 	arr, err := hex.DecodeString(s)
 	if err != nil {
 		panic(err)
 	}
-	var n number
-	var c uint64
-	for i := 0; i < len(arr); i++ {
-		var xi uint64
-		for j := 0; j < 8 && i < len(arr); j++ {
-			xi = xi << 8
-			xi = xi | uint64(arr[i])
-			i++
-		}
-		c = (xi & 0x8000000000000000) >> 63
-		xi = xi & 0x7fffffffffffffff
-		n = append(n, xi)
+	if len(arr) == 0 {
+		return z[:0]
 	}
-	*x = n
-	return x
+	m := int(math.Ceil(8 * float64(len(arr)) / 63))
+	if cap(z) < m {
+		z = make(number, m)
+	}
+	z = z[:m]
+	for _, b := range arr {
+		z = z.lsh(z, 8)
+		z[0] = z[0] | uint64(b)
+	}
+	return z.norm()
+}
+
+func (z number) lsh(x number, s uint) number {
+	t := uint8(s % 63)
+	blocks := (int(s) - int(t)) / 63
+	w := z.lsh63(x, t)
+	if cap(z) < len(w)+blocks {
+		z = make(number, len(w)+blocks)
+	}
+	z = z[:len(w)+blocks]
+	for i := 0; i < len(w); i++ {
+		z[i+blocks] = w[i]
+	}
+	return z
 }
 
 // left shift by i <= 63
-func (z *number) lsh63(x *number, s uint8) *number {
+func (z number) lsh63(x number, s uint8) number {
 	var bot, top uint64
 	mask := (((uint64(1) << s) - 1) << (63 - s))
-	if z == nil || cap(*z) < len(*x)+1 {
-		z = new(number)
-		*z = make(number, len(*x)+1)
+	if z == nil || cap(z) < len(x)+1 {
+		z = make(number, len(x)+1)
 	}
-	*z = (*z)[:len(*x)+1]
-	for i, xi := range *x {
+	z = z[:len(x)+1]
+	for i, xi := range x {
 		top = xi & mask // top s bits
-		fmt.Printf("top = %b", top)
-		fmt.Printf("bot = %b", bot)
-		(*z)[i] = ((xi << s) | bot) & 0x7fffffffffffffff
+		z[i] = ((xi << s) | bot) & 0x7fffffffffffffff
 		bot = top >> (63 - s) // bottom s bits
 	}
 	if bot != 0 {
-		*z = append(*z, bot)
+		z = append(z, bot)
 	}
-	if (*z)[len(*z)-1] == 0 {
-		*z = (*z)[:len(*z)-1]
+	if z[len(z)-1] == 0 {
+		z = z[:len(z)-1]
 	}
 	return z
 }
@@ -62,7 +71,10 @@ func (x number) add(y, z number) number {
 	}
 	// yLength >= zLength
 	var c uint64
-	x = make([]uint64, yLength+1) // TODO: use x's slice rather than new one
+	if cap(x) < yLength+1 {
+		x = make(number, yLength+1) // TODO: use x's slice rather than new one
+	}
+	x = x[:yLength+1]
 	i := 0
 	for ; i < zLength; i++ {
 		x[i] = y[i] + z[i] + c
@@ -79,4 +91,14 @@ func (x number) add(y, z number) number {
 		x = x[:i]
 	}
 	return x
+}
+
+func (z number) norm() number {
+	if z == nil {
+		return number{}
+	}
+	for i := len(z) - 1; i >= 0 && z[i] == 0; i-- {
+		z = z[:i]
+	}
+	return z
 }
--- number/natural_test.go
+++ number/natural_test.go
@@ -1,7 +1,6 @@
 package number
 
 import (
-	"math"
 	"math/big"
 	"reflect"
 	"testing"
@@ -33,45 +32,77 @@ func Test_number_add(t *testing.T) {
 	}
 }
 
-func Benchmark_number_add(b *testing.B) {
-	var x number
-	y := number{0, 0x7fffffffffffffff}
-	z := number{0, 0x7fffffffffffffff - 1}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		x.add(y, z)
-	}
-}
-
-func Benchmark_big_add(b *testing.B) {
-	x := new(big.Int)
-	y := new(big.Int).Lsh(big.NewInt(math.MaxInt64), 64)
-	z := new(big.Int).Lsh(big.NewInt(math.MaxInt64-1), 64)
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		x.Add(y, z)
-	}
-}
-
 func Test_number_setHex(t *testing.T) {
 	type args struct {
 		s string
 	}
 	tests := []struct {
 		name string
-		x    *number
+		z    number
 		args args
-		want *number
+		want number
 	}{
-		// TODO: Add test cases.
-		{"1", new(number), args{"01"}, &number{1}},
-		{"16", new(number), args{"10"}, &number{16}},
+		{"1", nil, args{"01"}, number{1}},
+		{"0", nil, args{"00"}, number{}},
+		{"7fffffffffffffff", nil, args{"7fffffffffffffff"}, number{0x7fffffffffffffff}},
+		{"ffffffffffffffff", nil, args{"ffffffffffffffff"}, number{0x7fffffffffffffff, 1}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.x.setHex(tt.args.s); !reflect.DeepEqual(*got, *tt.want) {
-				t.Errorf("number.setHex() = %v, want %v", *got, *tt.want)
+			if got := tt.z.setHex(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("number.setHex() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func Test_number_lsh(t *testing.T) {
+	type args struct {
+		x number
+		s uint
+	}
+	tests := []struct {
+		name string
+		z    number
+		args args
+		want number
+	}{
+		{"1<<1", nil, args{number{1}, 1}, number{1 << 1}},
+		{"1<<63", nil, args{number{1}, 63}, number{0, 1}},
+		{"1<<64", nil, args{number{1}, 64}, number{0, 1 << 1}},
+		{"2<<64", nil, args{number{2}, 64}, number{0, 2 << 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.z.lsh(tt.args.x, tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("number.lsh() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Benchmark_number_add(b *testing.B) {
+	x := number{}.setHex("ffffffffffffffafffffffffffffffffffffffffffffffffffff")
+	y := number{}.setHex("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
+	z := number{}.add(x, y)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		z.add(x, y)
+	}
+}
+
+func Benchmark_big_add(b *testing.B) {
+	x, ok := new(big.Int).SetString("ffffffffffffffafffffffffffffffffffffffffffffffffffff", 16)
+	if !ok {
+		panic("uh oh")
+	}
+	y, ok := new(big.Int).SetString("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 16)
+	if !ok {
+		panic("uh oh")
+	}
+	z := new(big.Int).Add(x, y)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		z.Add(x, y)
 	}
 }
